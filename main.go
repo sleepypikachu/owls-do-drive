ad3747981d56b5473baf6efc8be700e5d54e1867
Elliot Iddon <elliot@citrusbytes.net>
2016-09-03 13:54:30 +0100

    Add gin 'hello world'
    Just trying to make go get work so I've put the ping-pong example from
    the readme in.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import "net/http"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
